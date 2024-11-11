474a0c9f19d924ba4e5f953ea18b159ee9029495
prabutama <isaprabutama@gmail.com>
2024-11-11 09:15:57 +0700

    Fix problem at step 4-5

--- pa-game/src/game/obj/Effect.java
+++ pa-game/src/game/obj/Effect.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package game.obj;
 
 import java.awt.AlphaComposite;
@@ -12,11 +8,8 @@ import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
 import java.util.Random;
 
-/**
- *
- * @author maula
- */
 public class Effect {
+
     private final double x;
     private final double y;
     private final double max_distance;
@@ -38,29 +31,29 @@ public class Effect {
         this.color = color;
         createRandom();
     }
-    
+
     private void createRandom() {
         booms = new ModelBoom[totalEffect];
         float per = 360f / totalEffect;
         Random ran = new Random();
-        for (int i = 1; 1 <= totalEffect; i++) {
-            int r = ran.nextInt((int)per) + 1;
+        for (int i = 1; i <= totalEffect; i++) {
+            int r = ran.nextInt((int) per) + 1;
             int boomSize = ran.nextInt(max_size) + 1;
-            float angle=i * per + r;
+            float angle = i * per + r;
             booms[i - 1] = new ModelBoom(boomSize, angle);
         }
     }
-    
+
     public void draw(Graphics2D g2) {
         AffineTransform oldTransform = g2.getTransform();
         Composite oldComposite = g2.getComposite();
         g2.setColor(color);
         g2.translate(x, y);
-        for (ModelBoom b:booms) {
-            double bx=Math.cos(Math.toRadians(b.getAngle()))*current_distance;
-            double by=Math.sin(Math.toRadians(b.getAngle()))*current_distance;
-            double boomSize =  b.getSize();
-            double space = boomSize/2;
+        for (ModelBoom b : booms) {
+            double bx = Math.cos(Math.toRadians(b.getAngle())) * current_distance;
+            double by = Math.sin(Math.toRadians(b.getAngle())) * current_distance;
+            double boomSize = b.getSize();
+            double space = boomSize / 2;
             if (current_distance >= max_distance - (max_distance * 0.7f)) {
                 alpha = (float) ((max_distance - current_distance) / (max_distance * 0.7f));
             }
@@ -75,11 +68,11 @@ public class Effect {
         g2.setComposite(oldComposite);
         g2.setTransform(oldTransform);
     }
-    
+
     public void update() {
         current_distance += speed;
     }
-    
+
     public boolean check() {
         return current_distance < max_distance;
     }
